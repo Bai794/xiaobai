27ce470f3f03f862ecb9e5a2d98b931dcf48c186
Bai794 <44940731+Bai794@users.noreply.github.com>
2018-12-16 15:05:25 +0800

    C语言

--- main.c
+++ main.c
@@ -3,17 +3,27 @@
 
 int main()
 {
-    int n,i,a,sum=0;
-    printf("input n\n");
-    scanf("%ld",&n);
-    for(i=1;;i++){
-       a=i*i*i;
-       sum+=a;
-       if(sum>=n){
-        printf("%d",i-1);
-        break;
-       }
+   float a,b;
+   char op,x;
+  STAR: printf("Please enter the expression:\n");
+    scanf("%f %c%f",&a,&op,&b );
+   switch(op)
+   {
+   case'+':
+    printf("%f + %f = %f\n",a,b,a+b);break;
+   case'-':
+    printf("%f - %f = %f\n",a,b,a-b);break;
+   case'*':
+    printf("%f * %f = %f\n",a,b,a*b);break;
+   case'/':
+       if(b==0) printf("Division by zero!\n");
+       else printf("%f  %f = %f\n",a,b,a/b);break;
+   default:
+    printf("Unknown operator!\n");
+   }
+   printf("Do you want to continue(Y/N or y/n)?");
+   scanf(" %c",&x);
+   if(x=='y'||x=='Y') goto STAR;
+        else return;
 
-    }
-
-}
+   }
